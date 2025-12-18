62ae67c2e9d03bf01a9a23c670070a550e269230
fem5vel <fem5vel@gmail.com>
2025-12-18 21:28:09 +0300

    Added decryption in POST request

--- src_lib/server_http/ox_server_http_updater.c
+++ src_lib/server_http/ox_server_http_updater.c
@@ -15,9 +15,12 @@
 
 // ox
 #include "server_http/ox_server_http_updater.h"
+
+#include <config/app_config_crypto.h>
+
 #include "fwinfo/ox_fwinfo.h"
 #include "ox_asset_updater.h"
-
+#include <mbedtls/ccm.h>
 
 
 //
@@ -33,7 +36,16 @@
 #define FLASH_START_ADDR (FLASH_BASE + FLASH_BANK_SIZE)
 #define FLASH_END_ADDR (FLASH_BASE + FLASH_BANK_SIZE)
 
+#define AES_KEY_SIZE 32
+#define AES_NONCE_SIZE 8
+#define AES_MAC_SIZE 8
+#define AES_BLOCK_SIZE 32
 
+static mbedtls_ccm_context g_aes_ccm_ctx;
+static bool g_aes_ccm_initialized = false;
+static uint8_t g_decrypt_buf[AES_BLOCK_SIZE + AES_NONCE_SIZE + AES_MAC_SIZE];
+static size_t g_decrypt_buf_fill = 0;
+static uint64_t g_block_counter = 0;
 
 //
 // Private
@@ -46,11 +58,19 @@ static size_t g_app_flash_buf_fill = 0;
 static size_t _ox_server_updater_handler_upload(void* userdata, uint8_t* buf_out, size_t buf_out_size, const uint8_t* buf_in, size_t buf_in_len)
 {
     size_t result = 0U;
-
     (void)userdata;
 
     if (buf_in != NULL && g_app_flash_write_off == 0) {
         uni_hal_flash_erase_bank();
+
+        if (!g_aes_ccm_initialized) {
+            mbedtls_ccm_init(&g_aes_ccm_ctx);
+            if (mbedtls_ccm_setkey(&g_aes_ccm_ctx, MBEDTLS_CIPHER_ID_AES, g_ox_crypto_key, AES_KEY_SIZE * 8) != 0) {
+                return 0;
+            }
+            g_aes_ccm_initialized = true;
+            g_block_counter = 0;
+        }
     }
 
     // In case buf_out != nullptr -> we are finished -> write total count of bytes
@@ -58,21 +78,19 @@ static size_t _ox_server_updater_handler_upload(void* userdata, uint8_t* buf_out
     {
         int true_size =  g_app_flash_write_off + g_app_flash_buf_fill;
 
-        // check if we have any data to write
-        if (g_app_flash_buf_fill)
-        {
-            // pad
-            memset(&g_app_flash_buf[g_app_flash_buf_fill], 0xFF, FLASH_WRITE_ALIGN - g_app_flash_buf_fill);  // up to 32
-            if (uni_hal_flash_write(FLASH_START_ADDR + g_app_flash_write_off, FLASH_WRITE_ALIGN, g_app_flash_buf) != FLASH_WRITE_ALIGN)
-            {
-                 //printf("Flash write failed at offset %d\r\n", g_app_flash_write_off);
+        if (g_app_flash_buf_fill) {
+            memset(&g_app_flash_buf[g_app_flash_buf_fill], 0xFF, FLASH_WRITE_ALIGN - g_app_flash_buf_fill);
+            if (uni_hal_flash_write(FLASH_START_ADDR + g_app_flash_write_off, FLASH_WRITE_ALIGN, g_app_flash_buf) != FLASH_WRITE_ALIGN) {
+                //uni_hal_io_stdio_printf("Flash write failed at offset %d\r\n", g_app_flash_write_off);
             }
         }
         
         g_app_flash_buf_fill  = 0;
         g_app_flash_write_off = 0;
+        g_decrypt_buf_fill = 0;
+        g_block_counter = 0;
 
-        if (true_size != (1 * 1024 * 1024)) {
+        if (true_size > (1.5 * 1024 * 1024)) {
             result = uni_hal_io_stdio_snprintf((char*)buf_out, buf_out_size, "FAIL: Invalid firmware size");
         } else {
             result = uni_hal_io_stdio_snprintf((char*)buf_out, buf_out_size, "%d", true_size);
@@ -83,45 +101,87 @@ static size_t _ox_server_updater_handler_upload(void* userdata, uint8_t* buf_out
     {
         size_t in_idx = 0;
 
-        // finish block from previous call
-        if (g_app_flash_buf_fill)
-        {
-            // copy needed data
-            size_t need = uni_common_math_min(FLASH_WRITE_ALIGN - g_app_flash_buf_fill, buf_in_len);
-            memcpy(&g_app_flash_buf[g_app_flash_buf_fill], &buf_in[in_idx], need);
-            g_app_flash_buf_fill += need;
-            in_idx               += need;
+        if (g_decrypt_buf_fill > 0) {
+            size_t need = uni_common_math_min(AES_BLOCK_SIZE + AES_NONCE_SIZE + AES_MAC_SIZE - g_decrypt_buf_fill, buf_in_len);
+            memcpy(&g_decrypt_buf[g_decrypt_buf_fill], &buf_in[in_idx], need);
+            g_decrypt_buf_fill += need;
+            in_idx += need;
 
-            if (uni_hal_flash_write(FLASH_START_ADDR + g_app_flash_write_off, FLASH_WRITE_ALIGN, g_app_flash_buf) != FLASH_WRITE_ALIGN)
-            {
-                //printf("Flash write failed at offset %d\r\n", g_app_flash_write_off);
-                return 0;
+            if (g_decrypt_buf_fill == AES_BLOCK_SIZE + AES_NONCE_SIZE + AES_MAC_SIZE) {
+                uint8_t nonce[AES_NONCE_SIZE];
+                uint8_t mac[AES_MAC_SIZE];
+                uint8_t decrypted[AES_BLOCK_SIZE];
+
+                memcpy(nonce, &g_decrypt_buf[0], AES_NONCE_SIZE);
+                memcpy(&decrypted[0], &g_decrypt_buf[AES_NONCE_SIZE], AES_BLOCK_SIZE);
+                memcpy(mac, &g_decrypt_buf[AES_NONCE_SIZE + AES_BLOCK_SIZE], AES_MAC_SIZE);
+
+                if (mbedtls_ccm_auth_decrypt(&g_aes_ccm_ctx, AES_BLOCK_SIZE, nonce, AES_NONCE_SIZE,
+                                             NULL, 0, &decrypted[0], &decrypted[0], mac, AES_MAC_SIZE) != 0) {
+                    return 0;
+                }
+
+                size_t flash_need = uni_common_math_min(FLASH_WRITE_ALIGN - g_app_flash_buf_fill, AES_BLOCK_SIZE);
+                memcpy(&g_app_flash_buf[g_app_flash_buf_fill], decrypted, flash_need);
+                g_app_flash_buf_fill += flash_need;
+
+                if (g_app_flash_buf_fill == FLASH_WRITE_ALIGN) {
+                    if (uni_hal_flash_write(FLASH_START_ADDR + g_app_flash_write_off, FLASH_WRITE_ALIGN, g_app_flash_buf) != FLASH_WRITE_ALIGN) {
+                        return 0;
+                    }
+                    g_app_flash_write_off += FLASH_WRITE_ALIGN;
+                    g_app_flash_buf_fill = 0;
+
+                    if (flash_need < AES_BLOCK_SIZE) {
+                        memcpy(g_app_flash_buf, &decrypted[flash_need], AES_BLOCK_SIZE - flash_need);
+                        g_app_flash_buf_fill = AES_BLOCK_SIZE - flash_need;
+                    }
+                }
+
+                g_decrypt_buf_fill = 0;
+                g_block_counter++;
             }
-            g_app_flash_write_off += FLASH_WRITE_ALIGN;
-            g_app_flash_buf_fill   = 0;
         }
 
         portYIELD();
 
-        // program whole blocks
-        while (in_idx + FLASH_WRITE_ALIGN <= buf_in_len)
-        {
-            if (uni_hal_flash_write(FLASH_START_ADDR + g_app_flash_write_off, FLASH_WRITE_ALIGN, (uint8_t *)&buf_in[in_idx]) != FLASH_WRITE_ALIGN) {
-                //printf("Flash write failed at offset %d\r\n", g_app_flash_write_off);
+        size_t encrypted_block_size = AES_BLOCK_SIZE + AES_NONCE_SIZE + AES_MAC_SIZE;
+        while (in_idx + encrypted_block_size <= buf_in_len) {
+            uint8_t nonce[AES_NONCE_SIZE];
+            uint8_t mac[AES_MAC_SIZE];
+            uint8_t decrypted[AES_BLOCK_SIZE];
+
+            memcpy(nonce, &buf_in[in_idx], AES_NONCE_SIZE);
+            memcpy(&decrypted[0], &buf_in[in_idx + AES_NONCE_SIZE], AES_BLOCK_SIZE);
+            memcpy(mac, &buf_in[in_idx + AES_NONCE_SIZE + AES_BLOCK_SIZE], AES_MAC_SIZE);
+
+            if (mbedtls_ccm_auth_decrypt(&g_aes_ccm_ctx, AES_BLOCK_SIZE, nonce, AES_NONCE_SIZE,
+                                         NULL, 0, &decrypted[0], &decrypted[0], mac, AES_MAC_SIZE) != 0) {
                 return 0;
             }
-            in_idx               += FLASH_WRITE_ALIGN;
-            g_app_flash_write_off += FLASH_WRITE_ALIGN;
-        }
 
+            in_idx += encrypted_block_size;
+
+            size_t decrypt_idx = 0;
+            while (decrypt_idx < AES_BLOCK_SIZE) {
+                size_t flash_need = uni_common_math_min(FLASH_WRITE_ALIGN - g_app_flash_buf_fill, AES_BLOCK_SIZE - decrypt_idx);
+                memcpy(&g_app_flash_buf[g_app_flash_buf_fill], &decrypted[decrypt_idx], flash_need);
+                g_app_flash_buf_fill += flash_need;
+                decrypt_idx += flash_need;
+
+                if (g_app_flash_buf_fill == FLASH_WRITE_ALIGN) {
+                    if (uni_hal_flash_write(FLASH_START_ADDR + g_app_flash_write_off, FLASH_WRITE_ALIGN, g_app_flash_buf) != FLASH_WRITE_ALIGN) {
+                        return 0;
+                    }
+                    g_app_flash_write_off += FLASH_WRITE_ALIGN;
+                    g_app_flash_buf_fill = 0;
+                }
+            }
+
+            g_block_counter++;
+        }
 
         portYIELD();
-
-        // cache remaining part
-        g_app_flash_buf_fill = buf_in_len - in_idx;
-        if (g_app_flash_buf_fill) {
-            memcpy(g_app_flash_buf, &buf_in[in_idx], g_app_flash_buf_fill);
-        }
     }
     return result;
 }
